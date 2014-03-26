7a80bc72892f9b413587c3fc480ac3e8fbd88dcb
Sindre Moen <sindrenm@gmail.com>
2014-03-26 21:19:33 +0100

    Add support for subscribing to Instagram

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ exports.index = function(req, res) {
   res.render('index', { title: 'KF-pils Social Wall' });
 };
 
+exports.subscribe = function(req, res) {
+  res.send(req.query['hub.challenge']);
+};
+
 exports.callback = function(req, res) {
   req.io.broadcast('instagram', { photo: 'test' });
   res.send('Roger!');
